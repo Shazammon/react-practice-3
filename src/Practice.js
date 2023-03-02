73470316650b23d752d7b9b07e15505cad8fa360
Shazammon <ammonorgill@gmail.com>
2023-03-01 19:44:31 -0700

    set up tests

--- src/Practice.js
+++ src/Practice.js
@@ -2,14 +2,16 @@ let a = 10
 let b = "Hi"
 // let c = a
 let c = [1, 2]
-let d = c
+let d = [3,4,5]
 // c = c + 1
 
-// d.push(3)
+d.push(6)
 
 // d = [3, 4, 5]
 
-console.log(`a = ${a}`)
-console.log(`b = ${b}`)
-console.log(`c = ${c}`)
-console.log(`d = ${d}`)
+// console.log(`a = ${a}`)
+// console.log(`b = ${b}`)
+// console.log(`c = ${c}`)
+// console.log(`d = ${d}`)
+console.log(`c === d ${c === d}`)
+console.log(`c == d ${c == d}`)
